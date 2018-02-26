831b7de15777184356cd8d3e52c2b7184ff2de29
svdvovan <svdvovan@gmail.com>
2018-02-26 23:01:00 +0300

    begin

--- src/senza_losiny.java
+++ src/senza_losiny.java
@@ -11,9 +11,10 @@ public class senza_losiny {
         String Path = "https://www.senzarivali.ru/catalog/legginsy/";
 
         Document doc1 = Jsoup.connect(Path).get();
-        Elements lHref = doc1.select("a.d_block");
+     //   Elements lHref = doc1.select("a.d_block");
+        Elements lHref = doc1.getElementsByClass("item-title");
         Elements links1 = doc1.getElementsByClass("dark_link");
-
+        Elements addressUrl = lHref.select("a[href]");
         Elements prices = doc1.getElementsByClass("current_price got_bold pink");
         Elements Names = doc1.select("a[data-product]");
         Elements id_product = doc1.getElementsByClass("fast_view_block");
@@ -28,9 +29,9 @@ public class senza_losiny {
         int y = 0;
         for (Element link1 : links1) {
             String idNumber = id_product.get(y).attr("data-param-id");
-             System.out.println( idNumber+ " ; " + link1.text() + " ; " );
-//            String addressUrl = lHref.get(y).attr("abs:href");
-//            Document doc2 = Jsoup.connect(addressUrl).get();
+            String Url = addressUrl.get(y).attr("abs:href");
+             System.out.println( Url + " ; " +idNumber+ " ; " + link1.text() + " ; " );
+            Document doc2 = Jsoup.connect(Url).get();
 y++;
         }
 
