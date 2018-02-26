39210433a4dc2019e3c69cfbc8b3a4eb690d3b40
svdvovan <svdvovan@gmail.com>
2018-02-26 20:50:38 +0300

    begin

--- src/senza_losiny.java
+++ src/senza_losiny.java
@@ -13,17 +13,20 @@ public class senza_losiny {
         Document doc1 = Jsoup.connect(Path).get();
         Elements lHref = doc1.select("a.d_block");
         Elements links1 = doc1.getElementsByClass("dark_link");
-        Elements link1 = doc1.getElementsByClass("dark_link");
+
         Elements prices = doc1.getElementsByClass("current_price got_bold pink");
         Elements Names = doc1.select("a[data-product]");
-        System.out.print(  link1.text() + " ; " );
+
+
+
 
        int y = 0;
-//        for (Element link1 : links1) {
-  //          System.out.print( Names.get(y).attr("data-product") + " ; " + link1.text() + " ; " + prices.get(y).text() +" ; " );
-            String addressUrl = lHref.get(y).attr("abs:href");
-            Document doc2 = Jsoup.connect(addressUrl).get();
+        for (Element link1 : links1) {
+            System.out.println(  link1.text() + " ; " );
+//            String addressUrl = lHref.get(y).attr("abs:href");
+//            Document doc2 = Jsoup.connect(addressUrl).get();
+y++;
         }
 
-//    }
+   }
 }
