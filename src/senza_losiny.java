5a6791f5bb77bbf7b046a22f4ecf3b404191f0f1
svdvovan <svdvovan@gmail.com>
2018-02-26 20:24:27 +0300

    begin

--- src/senza_losiny.java
+++ src/senza_losiny.java
@@ -18,11 +18,11 @@ public class senza_losiny {
         Elements Names = doc1.select("a[data-product]");
         System.out.print(  link1.text() + " ; " );
 
-//        int y = 0;
+       int y = 0;
 //        for (Element link1 : links1) {
   //          System.out.print( Names.get(y).attr("data-product") + " ; " + link1.text() + " ; " + prices.get(y).text() +" ; " );
-//            String addressUrl = lHref.get(y).attr("abs:href");
-//            Document doc2 = Jsoup.connect(addressUrl).get();
+            String addressUrl = lHref.get(y).attr("abs:href");
+            Document doc2 = Jsoup.connect(addressUrl).get();
         }
 
 //    }
