7bb5a47f57191805ed7a7c189f96c3e19054a6af
svdvovan <svdvovan@gmail.com>
2018-02-26 20:20:15 +0300

    begin

---
+++ src/senza_losiny.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class senza_losiny {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("javax.net.ssl.trustStore", "d:/senzarivali.crt.jks");
+        String Path = "https://www.senzarivali.ru/catalog/legginsy/";
+
+        Document doc1 = Jsoup.connect(Path).get();
+        Elements lHref = doc1.select("a.d_block");
+        Elements links1 = doc1.getElementsByClass("dark_link");
+        Elements link1 = doc1.getElementsByClass("dark_link");
+        Elements prices = doc1.getElementsByClass("current_price got_bold pink");
+        Elements Names = doc1.select("a[data-product]");
+        System.out.print(  link1.text() + " ; " );
+
+//        int y = 0;
+//        for (Element link1 : links1) {
+  //          System.out.print( Names.get(y).attr("data-product") + " ; " + link1.text() + " ; " + prices.get(y).text() +" ; " );
+//            String addressUrl = lHref.get(y).attr("abs:href");
+//            Document doc2 = Jsoup.connect(addressUrl).get();
+        }
+
+//    }
+}
